c74e65b5b35d9a840bfb6ed35f6704de29293348
Theoretician9 <52958533+Theoretician9@users.noreply.github.com>
2025-05-04 01:28:40 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'src',                // исходная папка с index.html и main.tsx
+  root: 'src',                // говорим Vite, что точка входа — папка src
   base: '/',                  // базовый URL
   plugins: [react()],
   build: {
-    outDir: '../dist/client', // куда положить собранный фронтенд
+    outDir: '../dist/client', // собирать в dist/client
     emptyOutDir: true,        // очищать папку перед сборкой
   },
+  server: {
+    port: 5173,
+    host: true                // чтобы был доступ по сети (92.113.144.178)
+  }
 })
