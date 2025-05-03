0c150f0b97ead017487667b435b202d0e84246c7
Theoretician9 <52958533+Theoretician9@users.noreply.github.com>
2025-05-04 01:25:09 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'src',
+  root: 'src',                // исходная папка с index.html и main.tsx
+  base: '/',                  // базовый URL
   plugins: [react()],
   build: {
-    outDir: '../dist/client',
-    emptyOutDir: true
-  }
+    outDir: '../dist/client', // куда положить собранный фронтенд
+    emptyOutDir: true,        // очищать папку перед сборкой
+  },
 })
