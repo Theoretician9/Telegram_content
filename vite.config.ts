4a8fe157f84c652264f88af0972da8ca340438cd
Theoretician9 <52958533+Theoretician9@users.noreply.github.com>
2025-05-04 09:47:40 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,35 +3,25 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
+  // Точка входа проекта — там, где лежит index.html
   root: resolve(__dirname, 'src'),
+  plugins: [react()],
   build: {
+    // В папку dist/client
     outDir: resolve(__dirname, 'dist/client'),
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src', 'index.html'),
-      },
+      input: resolve(__dirname, 'src', 'index.html'),
+    },
+  },
+  resolve: {
+    // Разрешаем модули из корня проекта
+    alias: {
+      react: resolve(__dirname, 'node_modules', 'react'),
+      'react-dom': resolve(__dirname, 'node_modules', 'react-dom'),
     },
   },
   optimizeDeps: {
-    include: ['react/jsx-runtime', 'react/jsx-dev-runtime']
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // чтобы Vite корректно находил JSX-runtime в React 18
-      'react/jsx-runtime': resolve(
-        __dirname,
-        'node_modules',
-        'react',
-        'jsx-runtime.js'
-      ),
-      'react/jsx-dev-runtime': resolve(
-        __dirname,
-        'node_modules',
-        'react',
-        'jsx-dev-runtime.js'
-      ),
-    },
-  },
+    include: ['react', 'react-dom', 'react/jsx-runtime', 'react/jsx-dev-runtime']
+  }
 })
