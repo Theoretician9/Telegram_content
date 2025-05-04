c98234d34d37375949a36d427c4e00de7189f525
Theoretician9 <52958533+Theoretician9@users.noreply.github.com>
2025-05-04 21:08:40 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,37 +4,19 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  // Корневая папка, где лежит index.html
   root: resolve(__dirname, 'src'),
   plugins: [react()],
   resolve: {
     alias: {
-      // '~' будет указывать в папку src
+      // вот он, нужный алиас
       '~': resolve(__dirname, 'src'),
-      // если в коде используются абсолютные импорты из корня проекта, например:
-      // import { foo } from '/client/utils'
-      // то можно их тоже алиасить:
-      '/client': resolve(__dirname, 'src', 'client'),
-    }
+    },
   },
   build: {
-    // В какую папку собирать
     outDir: resolve(__dirname, 'dist/client'),
     emptyOutDir: true,
-    // Точка входа (относительно project root, а не workDir)
     rollupOptions: {
       input: resolve(__dirname, 'src', 'index.html'),
     },
-  },
-  optimizeDeps: {
-    include: [
-      'react',
-      'react-dom',
-      'react/jsx-runtime',
-      'react/jsx-dev-runtime',
-      'react-router-dom',
-      '@tanstack/react-query',
-      'lucide-react',
-    ],
   },
 })
