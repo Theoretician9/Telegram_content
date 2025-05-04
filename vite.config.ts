82ee99df4fee83820653adc998f8d2f49d5bc5a9
Theoretician9 <52958533+Theoretician9@users.noreply.github.com>
2025-05-04 19:38:38 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,21 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  // Точка входа — папка с index.html
+  // указываем, что точка входа — папка src
   root: resolve(__dirname, 'src'),
-  base: '/',
   plugins: [react()],
   resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
     alias: [
-      // чтобы ~/client/api резолвился в src/client/api
-      { find: '~', replacement: resolve(__dirname, 'src') },
-      // будем подставлять React из node_modules
-      { find: 'react', replacement: resolve(__dirname, 'node_modules/react') },
-      { find: 'react-dom', replacement: resolve(__dirname, 'node_modules/react-dom') },
-      // и jsx-runtime из файлов с .js-расширением
+      // без этого Vite не найдёт jsx-runtime в React 18
       {
         find: 'react/jsx-runtime',
-        replacement: resolve(__dirname, 'node_modules/react/jsx-runtime.js'),
+        replacement: resolve(__dirname, 'node_modules', 'react', 'jsx-runtime.js')
       },
       {
         find: 'react/jsx-dev-runtime',
-        replacement: resolve(
-          __dirname,
-          'node_modules/react/jsx-dev-runtime.js'
-        ),
-      },
-    ],
+        replacement: resolve(__dirname, 'node_modules', 'react', 'jsx-dev-runtime.js')
+      }
+    ]
   },
   optimizeDeps: {
     include: [
@@ -35,13 +25,18 @@ export default defineConfig({
       'react-dom',
       'react/jsx-runtime',
       'react/jsx-dev-runtime',
-    ],
+      'react-router-dom',
+      '@tanstack/react-query',
+      'lucide-react'
+    ]
   },
   build: {
+    // сборка в dist/client
     outDir: resolve(__dirname, 'dist/client'),
     emptyOutDir: true,
     rollupOptions: {
-      input: resolve(__dirname, 'src/index.html'),
-    },
-  },
+      // файл входа
+      input: resolve(__dirname, 'src', 'index.html')
+    }
+  }
 })
